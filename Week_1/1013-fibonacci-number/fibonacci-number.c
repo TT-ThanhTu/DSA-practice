2739a6da4ab5566a3e137e5c4e9f617d51847c10
Tran Thi Thanh Tu <thanhtu060505@gmail.com>
2026-06-16 21:39:51 +0700

    Time: 180 ms (11.29%) | Memory: 8.6 MB (1.68%) - LeetSync

--- Week_1/1013-fibonacci-number/fibonacci-number.c
+++ Week_1/1013-fibonacci-number/fibonacci-number.c
@@ -1,5 +1,12 @@
 
 int fib(int n){
-    if (n<=1) return n;
-    return fib(n-1)+fib(n-2);
+    int a[n+3]; 
+    for(int i = 0; i <= n; i++) a[i]=-1;
+    if (n<=1) return a[n] = n;
+    /// 0 1 1 2 3 5 8
+    else{
+        if(a[n] != -1) return a[n];
+        else a[n] = fib(n-1) + fib(n-2); 
+    }
+    return a[n];
 }
