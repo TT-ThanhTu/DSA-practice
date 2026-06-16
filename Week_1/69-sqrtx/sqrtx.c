b57dff1f81a022f1ba4cd1e4dcb3e14a19a79908
Tran Thi Thanh Tu <thanhtu060505@gmail.com>
2026-06-16 18:18:49 +0700

    Time: 0 ms (100.00%) | Memory: 9.2 MB (51.38%) - LeetSync

--- Week_1/69-sqrtx/sqrtx.c
+++ Week_1/69-sqrtx/sqrtx.c
@@ -1,10 +1,21 @@
 int mySqrt(int x) {
-    if (x == 0 || x == 1)
+    if (x < 2)
         return x;
 
-    for (long long i = 0; ; i++) {
-        if (i * i > x) {
-            return (int)(i - 1);
+    long long left = 1;
+    long long right = x / 2;
+    int ans = 0;
+
+    while (left <= right) {
+        long long mid = left + (right - left) / 2;
+
+        if (mid * mid <= x) {
+            ans = (int)mid;
+            left = mid + 1;
+        } else {
+            right = mid - 1;
         }
     }
+
+    return ans;
 }
