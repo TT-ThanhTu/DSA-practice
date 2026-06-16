f8b505684805044960e17398e1829ec18d5dfd52
Tran Thi Thanh Tu <thanhtu060505@gmail.com>
2026-06-16 21:09:13 +0700

    Time: 3 ms (40.71%) | Memory: 8.4 MB (86.86%) - LeetSync

--- Week_1/507-perfect-number/perfect-number.c
+++ Week_1/507-perfect-number/perfect-number.c
@@ -1,16 +1,17 @@
 bool checkPerfectNumber(int num) {
+    double a= sqrt(num);
     if (num<=1) {
          return false;
     }
     int sum =0;
-    for (int i=1; i<=num/2; i++){
+    for (int i=1; i<a; i++){
         if (num%i==0){
-            sum+=i;
+            int b= num/i;
+            sum+=i+b;
         }
     }
-    if (sum==num){
+    if ((sum-num)==num){
         return true;
     }
     return false;
-    
 }
