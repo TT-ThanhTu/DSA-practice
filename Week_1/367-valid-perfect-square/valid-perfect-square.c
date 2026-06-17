244072187ba145984f7e30ebbd791913a9daa2b0
Tran Thi Thanh Tu <thanhtu060505@gmail.com>
2026-06-17 21:37:55 +0700

    Time: 0 ms (100.00%) | Memory: 8.4 MB (59.59%) - LeetSync

--- Week_1/367-valid-perfect-square/valid-perfect-square.c
+++ Week_1/367-valid-perfect-square/valid-perfect-square.c
@@ -1,7 +1,6 @@
 bool isPerfectSquare(int num) {
-    if (num==1) return true;
-    for (long long i=2;i<num ;i++){
-        if ((i*i)==num) return true;
+    for (long long i = 1; i * i <= num; i++) {
+        if (i * i == num) return true;
     }
     return false;
 }
