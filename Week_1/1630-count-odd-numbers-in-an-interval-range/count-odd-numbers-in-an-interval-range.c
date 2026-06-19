d64e6d6f3902109d47937ccdd2c87207ed2c6ee8
Tran Thi Thanh Tu <thanhtu060505@gmail.com>
2026-06-19 22:37:01 +0700

    Time: 0 ms (100.00%) | Memory: 8.6 MB (38.83%) - LeetSync

--- Week_1/1630-count-odd-numbers-in-an-interval-range/count-odd-numbers-in-an-interval-range.c
+++ Week_1/1630-count-odd-numbers-in-an-interval-range/count-odd-numbers-in-an-interval-range.c
@@ -5,7 +5,7 @@ int countOdds(int low, int high){
     // 2  3 5 7     8   // 8-2/2 +1=4
 
     int s=((high-low)/2)+1;
-    if (high%2==0 && low%2==0){
+    if (low%2==0 && high%2==0){
         return s-1;
     }
 
