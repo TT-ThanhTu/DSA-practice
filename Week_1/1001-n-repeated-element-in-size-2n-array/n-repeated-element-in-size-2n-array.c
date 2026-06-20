4fc24dcb2cd06b753b4425671796d322b48186f3
Tran Thi Thanh Tu <thanhtu060505@gmail.com>
2026-06-20 22:54:29 +0700

    Time: 0 ms (100.00%) | Memory: 11 MB (25.58%) - LeetSync

--- Week_1/1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.c
+++ Week_1/1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.c
@@ -1,17 +1,36 @@
 int repeatedNTimes(int* nums, int numsSize) {
+    int a[10000+1]={0};
     int maxCount = 0;
-    int result = nums[0];
+    int result, b;
     for(int i=0;i<numsSize; i++){
-        int count=0;
-        for(int j=i; j<numsSize; j++){
-            if(nums[i]==nums[j]){
-                count++;
-            }
-        }
-        if(count > maxCount){
-            maxCount = count;
+        a[nums[i]]++;
+        b=a[nums[i]];
+        if(b>maxCount){
+            maxCount=b;
             result = nums[i];
         }
     }
-   return result;
+    return result;
 }
+
+
+
+
+
+
+
+//     int maxCount = 0;
+//     int result = nums[0];
+//     for(int i=0;i<numsSize; i++){
+//         int count=0;
+//         for(int j=i; j<numsSize; j++){
+//             if(nums[i]==nums[j]){
+//                 count++;
+//             }
+//         }
+//         if(count > maxCount){
+//             maxCount = count;
+//             result = nums[i];
+//         }
+//     }
+//    return result;
