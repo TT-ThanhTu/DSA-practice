b221415048de1e335bd339dfa6545dc24c146a68
Tran Thi Thanh Tu <thanhtu060505@gmail.com>
2026-06-27 17:13:35 +0700

    Time: 0 ms (100.00%) | Memory: 8.4 MB (95.18%) - LeetSync

---
+++ Week_2/4275-traffic-signal-color/traffic-signal-color.c
@@ -0,0 +1,14 @@
+char* trafficSignal(int timer) {
+    if(timer==0){
+        return "Green";
+    }
+    if(timer==30){
+        return "Orange";
+    }
+    if(timer>30 && timer<=90){
+        return "Red";
+    }
+    return "Invalid";
+
+    
+}
