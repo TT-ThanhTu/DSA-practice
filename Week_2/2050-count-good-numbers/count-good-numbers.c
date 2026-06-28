cceea0116495135e6869dc321ff92283135ddf18
Tran Thi Thanh Tu <thanhtu060505@gmail.com>
2026-06-28 21:50:01 +0700

    Time: 0 ms (100.00%) | Memory: 8.7 MB (35.92%) - LeetSync

---
+++ Week_2/2050-count-good-numbers/count-good-numbers.c
@@ -0,0 +1,22 @@
+#define MOD 1000000007LL
+
+long long power(long long a, long long b){
+    long long res = 1;
+
+    while(b){
+        if(b & 1)
+            res = (res * a) % MOD;
+
+        a = (a * a) % MOD;
+        b >>= 1;
+    }
+
+    return res;
+}
+
+int countGoodNumbers(long long n) {
+    long long even = (n + 1) / 2;
+    long long odd = n / 2;
+
+    return (power(5, even) * power(4, odd)) % MOD;
+}
