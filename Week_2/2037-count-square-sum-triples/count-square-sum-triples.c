3b007bc015306baf7e6d66514a6e2661e2fa190a
Tran Thi Thanh Tu <thanhtu060505@gmail.com>
2026-06-29 22:13:42 +0700

    Time: 4 ms (63.46%) | Memory: 8.3 MB (96.15%) - LeetSync

--- Week_2/2037-count-square-sum-triples/count-square-sum-triples.c
+++ Week_2/2037-count-square-sum-triples/count-square-sum-triples.c
@@ -1,12 +1,16 @@
+#include <math.h>
+
 int countTriples(int n) {
     int count = 0;
 
     for (int a = 1; a <= n; a++) {
         for (int b = 1; b <= n; b++) {
-            for (int c = 1; c <= n; c++) {
-                if (a * a + b * b == c * c)
-                    count++;
-            }
+
+            int sum = a * a + b * b;
+            int c = (int)sqrt(sum);
+
+            if (c <= n && c * c == sum)
+                count++;
         }
     }
 
