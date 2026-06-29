970db65a58bdbb26c633be6de0b07742f7a7cacd
Tran Thi Thanh Tu <thanhtu060505@gmail.com>
2026-06-29 23:38:16 +0700

    Time: 0 ms (100.00%) | Memory: 12 MB (52.67%) - LeetSync

--- Week_2/1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.c
+++ Week_2/1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.c
@@ -1,18 +1,27 @@
+
+
 int* smallerNumbersThanCurrent(int* nums, int numsSize, int* returnSize) {
-    int *a = (int *)malloc(numsSize * sizeof(int));
+    int cnt[101] = {0};
+
+    // Đếm số lần xuất hiện
+    for (int i = 0; i < numsSize; i++) {
+        cnt[nums[i]]++;
+    }
+
+    // Tổng tích lũy
+    for (int i = 1; i <= 100; i++) {
+        cnt[i] += cnt[i - 1];
+    }
+
+    int *ans = (int*)malloc(numsSize * sizeof(int));
 
     for (int i = 0; i < numsSize; i++) {
-        int count = 0;
-
-        for (int j = 0; j < numsSize; j++) {
-            if (nums[j] < nums[i]) {
-                count++;
-            }
-        }
-
-        a[i] = count;
+        if (nums[i] == 0)
+            ans[i] = 0;
+        else
+            ans[i] = cnt[nums[i] - 1];
     }
 
     *returnSize = numsSize;
-    return a;
+    return ans;
 }
