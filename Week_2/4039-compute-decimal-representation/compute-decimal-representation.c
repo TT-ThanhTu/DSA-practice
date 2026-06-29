d6b478ebede214983f6a793a5ec4159e9942f15e
Tran Thi Thanh Tu <thanhtu060505@gmail.com>
2026-06-29 21:56:03 +0700

    Time: 11 ms (0.00%) | Memory: 20.9 MB (6.25%) - LeetSync

---
+++ Week_2/4039-compute-decimal-representation/compute-decimal-representation.c
@@ -0,0 +1,29 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* decimalRepresentation(int n, int* returnSize) {
+    int *a= (int*) malloc(n*sizeof(int));
+    int i=0;
+    long long power=1;
+    while (n > 0) {
+
+    int digit = n%10;
+
+    if (digit != 0){
+        a[i] = digit*power;
+         i++;
+    }
+    power *= 10;
+    n /= 10;
+}
+   for (int left = 0, right = i - 1; left < right; left++, right--) {
+    int temp = a[left];
+    a[left] = a[right];
+    a[right] = temp;
+}
+
+*returnSize = i;
+
+return a;
+    
+}
