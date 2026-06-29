a8c80e809b63f42949bb65f97f678cfacee97ca8
Tran Thi Thanh Tu <thanhtu060505@gmail.com>
2026-06-29 23:46:44 +0700

    Time: 4 ms (40.74%) | Memory: 9.7 MB (62.96%) - LeetSync

--- Week_2/3367-find-the-sum-of-encrypted-integers/find-the-sum-of-encrypted-integers.c
+++ Week_2/3367-find-the-sum-of-encrypted-integers/find-the-sum-of-encrypted-integers.c
@@ -1,28 +1,29 @@
-int encrypt(int x) {
-    int t = x;
-    int mx = 0;
-    int len = 0;
-
-    while (t) {
-        if (t % 10 > mx)
-            mx = t % 10;
-        len++;
-        t /= 10;
-    }
-
-    int ans = 0;
-    while (len--) {
-        ans = ans * 10 + mx;
-    }
-
-    return ans;
-}
-
 int sumOfEncryptedInt(int* nums, int numsSize) {
     int sum = 0;
 
     for (int i = 0; i < numsSize; i++) {
-        sum += encrypt(nums[i]);
+        int x = nums[i];
+        int temp = x;
+        int maxDigit = 0;
+        int encrypted = 0;
+        int base = 1;
+
+        // Tìm chữ số lớn nhất
+        while (temp > 0) {
+            if (temp % 10 > maxDigit)
+                maxDigit = temp % 10;
+            temp /= 10;
+        }
+
+        // Tạo số đã mã hóa
+        temp = x;
+        while (temp > 0) {
+            encrypted += maxDigit * base;
+            base *= 10;
+            temp /= 10;
+        }
+
+        sum += encrypted;
     }
 
     return sum;
