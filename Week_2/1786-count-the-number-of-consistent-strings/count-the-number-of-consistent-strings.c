794bdf544e93834c2509da13fe486cbfb1b0117d
Tran Thi Thanh Tu <thanhtu060505@gmail.com>
2026-06-30 13:54:48 +0700

    Time: 50 ms (14.57%) | Memory: 14.9 MB (99.34%) - LeetSync

--- Week_2/1786-count-the-number-of-consistent-strings/count-the-number-of-consistent-strings.c
+++ Week_2/1786-count-the-number-of-consistent-strings/count-the-number-of-consistent-strings.c
@@ -1,29 +1,49 @@
-
-int countConsistentStrings(char * allowed, char ** words, int wordsSize){
-    // bai nay khong duoc chua ky tu khac
-    int a[26] = {0};
-    while (*allowed) {
-        a[*allowed - 'a'] = 1;
-        allowed++;
-    }
-
+int countConsistentStrings(char *allowed, char **words, int wordsSize) {
     int count = 0;
 
     for (int i = 0; i < wordsSize; i++) {
         char *word = words[i];
-        int check = 1;
+        int ok = 1;
 
         while (*word) {
-            // Nếu gặp ký tự không thuộc allowed
-            if (a[*word - 'a'] == 0) {
-                check = 0;
+            if (strchr(allowed, *word) == NULL) {
+                ok = 0;
                 break;
             }
             word++;
         }
-        if (check)
+
+        if (ok)
             count++;
     }
 
     return count;
 }
+// int countConsistentStrings(char * allowed, char ** words, int wordsSize){
+//     // bai nay khong duoc chua ky tu khac
+//     int a[26] = {0};
+//     while (*allowed) {
+//         a[*allowed - 'a'] = 1;
+//         allowed++;
+//     }
+
+//     int count = 0;
+
+//     for (int i = 0; i < wordsSize; i++) {
+//         char *word = words[i];
+//         int check = 1;
+
+//         while (*word) {
+//             // Nếu gặp ký tự không thuộc allowed
+//             if (a[*word - 'a'] == 0) {
+//                 check = 0;
+//                 break;
+//             }
+//             word++;
+//         }
+//         if (check)
+//             count++;
+//     }
+
+//     return count;
+// }
